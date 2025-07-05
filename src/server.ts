02549f5f5f6e76c5375564ea49e3262ec51408ca
p6h6di <shivamth060@gmail.com>
2025-07-06 00:51:00 +0530

    Add Express server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
-function test() {
-    console.log("This is a test function.");
-}
-test();
+import app from "./app";
+import { Config } from "./config";
+
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+startServer();
