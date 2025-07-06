0a8514d0ba47fc778380a78426abb69acd94543b
p6h6di <shivamth060@gmail.com>
2025-07-06 14:12:36 +0530

    Add http-errors dependency and implement global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Response, Request, NextFunction } from "express";
+import logger from "./config/logger";
+import { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello, Pizza Stack!");
+});
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.status || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: "",
+                location: "",
+            },
+        ],
+    });
 });
 
 export default app;
