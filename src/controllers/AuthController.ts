dc6034f5d076284e98a2da598123603e6c19f661
p6h6di <shivamth060@gmail.com>
2025-07-08 14:16:33 +0530

    feat: enhance user registration with error handling and logging

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,17 +1,31 @@
-import { Response } from "express";
+import { NextFunction, Response } from "express";
 import { RegisterRequestBody } from "../types";
 import { UserService } from "../services/UserService";
+import { Logger } from "winston";
 
 export class AuthController {
-    userService: UserService;
-    constructor(userService: UserService) {
-        this.userService = userService;
-    }
-    async register(req: RegisterRequestBody, res: Response) {
-        const { firstName, lastName, email, password } = req.body;
+    constructor(
+        private userService: UserService,
+        private logger: Logger
+    ) {}
+    async register(req: RegisterRequestBody, res: Response, next: NextFunction) {
+        try {
+            const { firstName, lastName, email, password } = req.body;
+            this.logger.debug("New user registration request", {
+                firstName,
+                lastName,
+                email,
+                password: "*******", // Do not log passwords
+            });
+            const user = await this.userService.create({ email, firstName, lastName, password });
+            this.logger.info("User has been created", {
+                id: user.id,
+            });
 
-        const user = await this.userService.create({ email, firstName, lastName, password });
-
-        res.status(201).json({ id: user.id });
+            res.status(201).json({ id: user.id });
+        } catch (error) {
+            next(error);
+            return;
+        }
     }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,17 +1,26 @@
 import { Repository } from "typeorm";
 import { User } from "../entity/User";
 import { UserData } from "../types";
+import createHttpError from "http-errors";
 
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
     async create({ email, firstName, lastName, password }: UserData) {
-        const user = await this.userRepository.save({
-            firstName,
-            lastName,
-            email,
-            password,
-        });
+        try {
+            const user = await this.userRepository.save({
+                firstName,
+                lastName,
+                email,
+                password,
+            });
 
-        return user;
+            return user;
+        } catch (err) {
+            const error = createHttpError(
+                500,
+                `Failed to store user in the database: ${err instanceof Error ? err.message : String(err)}`
+            );
+            throw error;
+        }
     }
 }
