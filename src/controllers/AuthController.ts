c14f7354401e4cded5cd28e340af3b2e068b1781
p6h6di <shivamth060@gmail.com>
2025-07-09 21:46:38 +0530

    feat: implement JWT authentication with access and refresh tokens

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,13 @@
+import fs from "fs";
+import path from "path";
 import { NextFunction, Response } from "express";
 import { RegisterRequestBody } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
 import { validationResult } from "express-validator";
+import { JwtPayload, sign } from "jsonwebtoken";
+import createHttpError from "http-errors";
+import { Config } from "../config";
 
 export class AuthController {
     constructor(
@@ -28,6 +33,45 @@ export class AuthController {
             const user = await this.userService.create({ email, firstName, lastName, password });
             this.logger.info("User has been created", {
                 id: user.id,
+            });
+
+            let privateKey: Buffer;
+            try {
+                privateKey = fs.readFileSync(path.join(__dirname, "../../certs/private.pem"));
+            } catch {
+                const error = createHttpError(500, "Error reading private key");
+                next(error);
+                return;
+            }
+
+            const payload: JwtPayload = {
+                sub: String(user.id),
+                role: user.role,
+            };
+
+            const accessToken = sign(payload, privateKey, {
+                algorithm: "RS256",
+                expiresIn: "1h",
+                issuer: "auth-service",
+            });
+            const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
+                algorithm: "HS256",
+                expiresIn: "1y",
+                issuer: "auth-service",
+            });
+
+            res.cookie("accessToken", accessToken, {
+                domain: "localhost",
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60, // 1 hour
+                httpOnly: true,
+            });
+
+            res.cookie("refreshToken", refreshToken, {
+                domain: "localhost",
+                sameSite: "strict",
+                maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+                httpOnly: true,
             });
 
             res.status(201).json({ id: user.id });
--- src/utils.ts
+++
@@ -1,3 +0,0 @@
-export const calculateDiscount = (price: number, discount: number): number => {
-    return price - (price * discount) / 100;
-};
